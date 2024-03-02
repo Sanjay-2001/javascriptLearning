0992d133b8a50eafb74bae61dbee154274d0c00f
Sanjay-2001 <90711694+Sanjay-2001@users.noreply.github.com>
2024-03-02 04:24:34 +0000

    data types study

---
+++ .devcontainer/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+//alert("hello")
+
+console.log("hello");
+
+let score = undefined;
+console.log(typeof score);
+
+let valueInNumber = Number(score);
+console.log("value>>>>>>>>>>>", valueInNumber);
+console.log(typeof( valueInNumber));
+
+
+//"33" => 33
+//"33abc" => NaN
+//"null" => 0
+//true || false => 1 || 0
+//undefined => NaN
